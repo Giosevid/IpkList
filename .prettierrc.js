353155a56a6b31a7fcc5abf89f615241b78169a9
Giosevid Acosta <giosevid@gmail.com>
2023-05-05 18:22:54 +0200

    husky configured

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,11 @@
 module.exports = {
-  arrowParens: 'avoid',
+  arrowParens: "avoid",
   bracketSameLine: true,
   bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
+  singleQuote: false,
+  trailingComma: "all",
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  printWidth: 80,
 };
