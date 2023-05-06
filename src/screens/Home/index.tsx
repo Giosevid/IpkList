3ac5e8de652ee79f9ded6f4ce7c296acc874fba7
Giosevid Acosta <giosevid@gmail.com>
2023-05-06 10:53:08 +0200

    Endpoint and response data created

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,45 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { FlatList, View } from 'react-native';
+import { Button, Card, Text } from 'react-native-paper';
+import { useGetStoresQuery } from '../../services/api';
+import { Stores } from 'IpkList/src/types/stores';
+
+type ItemProps = {
+  item: Stores;
+};
 
 const Home = () => {
-  return (
-    <View style={{backgroundColor: 'blue', flex: 1}}>
-      <Text>¡Hola mundo!</Text>
-    </View>
+  const { data: stores, isLoading } = useGetStoresQuery('');
+
+  console.log('isLoading', isLoading);
+
+  console.log('stores', stores);
+
+  if (isLoading) {
+    return (
+      <View>
+        <Text>Cargando...</Text>
+      </View>
+    );
+  }
+
+  const renderItem = ({ item }: ItemProps) => (
+    <Card>
+      <Card.Title title={item.name} />
+      <Card.Content>{/* <Text>{item.description}</Text> */}</Card.Content>
+      <Card.Actions>
+        <Button>Ver</Button>
+      </Card.Actions>
+    </Card>
   );
-}
+
+  return (
+    <FlatList
+      data={stores}
+      renderItem={renderItem}
+      keyExtractor={item => item.id}
+    />
+  );
+};
 
 export default Home;
---
+++ src/services/api.ts
@@ -0,0 +1,35 @@
+import {
+  BaseQueryFn,
+  FetchArgs,
+  createApi,
+  fetchBaseQuery,
+  FetchBaseQueryError,
+} from '@reduxjs/toolkit/query/react';
+
+const baseQuery = fetchBaseQuery({
+  // TODO: Pasar esto a el .env
+  baseUrl: 'https://ikp-mobile-challenge-backend.up.railway.app',
+});
+
+const baseQueryWithInterceptor: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions);
+  if (result.error && result.error.status === 401) {
+  }
+  return result;
+};
+
+export const api = createApi({
+  reducerPath: 'api',
+  baseQuery: baseQueryWithInterceptor,
+  endpoints: builder => ({
+    getStores: builder.query({
+      query: () => '/stores',
+    }),
+  }),
+});
+
+export const { useGetStoresQuery } = api;
